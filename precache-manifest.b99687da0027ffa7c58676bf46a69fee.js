d83e844223e1ddb8d011877557f643f528a68dbf
Patrick O'Brien <patricknathanobrien@gmail.com>
2020-06-24 20:44:46 -0400

    Updates

---
+++ precache-manifest.b99687da0027ffa7c58676bf46a69fee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee10343b227a9282c4bd579b994a0f3",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "7c91db0ffd95baa5a8f1",
+    "url": "/clickygame/static/js/2.ac31fbff.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/clickygame/static/js/2.ac31fbff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7cd3adf582f46747dca",
+    "url": "/clickygame/static/js/main.9b208f4b.chunk.js"
+  },
+  {
+    "revision": "7122649522b9616ddb97",
+    "url": "/clickygame/static/js/runtime-main.71d67497.js"
+  }
+]);
